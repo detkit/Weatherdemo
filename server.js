b4da531220b5bff837df7817df0be43fd5c9a5e9
Quang 166Dev <quangthat97@gmail.com>
2018-07-13 20:41:33 +0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-    if(req.headers['x-forwarded-proto' === 'http']) {
-      next();
-    } else {
-      res.redirect('http://' + req.hostname + req.url);
-    }
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
 });
 
 app.use(express.static('public'));
-
 
 app.listen(PORT, function () {
   console.log('Express server is up on port' + PORT);
