43fd8044cbd0b4380c4481d2e37c90a2e33ed6b7
Quang 166Dev <quangthat97@gmail.com>
2018-07-13 21:07:05 +0700

    Rename example page

--- public/bundle.js
+++ public/bundle.js
@@ -26770,9 +26770,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to examples page!'
+	    )
 	  );
 	};
 	module.exports = Examples;
